79ed8b8be9a1d2a9355299e931f43fa5b65b097b
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-04-16 20:51:38 -0500

    nt9 feat se inicia proyecto-integrador

---
+++ ambental/src/Home/Home.jsx
@@ -0,0 +1,12 @@
+import "./Home.css"
+
+export function Home (){
+    return(
+        <>
+            <section className="banner text-white">
+                <h2>VERDEO APP</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate beatae dicta, quas sint atque, molestiae, provident eveniet doloribus nemo eum vero reiciendis quae modi earum amet non. Harum saepe architecto omnis qui vitae rem amet nulla fuga officia ipsa laboriosam rerum minima corporis quasi esse, accusantium in incidunt quisquam repellat natus commodi. Velit praesentium corrupti alias ipsa ducimus ad suscipit tempora vel fuga, esse ipsam quasi? Aspernatur, officiis ea. Doloremque.</p>
+            </section>  
+        </>
+    )
+}
